2f42b8c84e76dd33026d1913e51f39b001627cb7
Vladyslav Popov <sk3ally@gmail.com>
2019-10-17 13:10:16 +0300

    Исправление

--- InternshipTest/src/institution/University.java
+++ InternshipTest/src/institution/University.java
@@ -3,42 +3,42 @@ package institution;
 import person.Student;
 import person.consciousness.Knowledge;
 
-import java.util.ArrayList; //Подключаем метод List, для работы со массивами списков
+import java.util.ArrayList;
 import java.util.List;
 
 public class University {
-    private List<Student> students = new ArrayList<>(); //Создаем массив для пременной
-    private String name; //Оглашаем переменную
+    private List<Student> students = new ArrayList<>();
+    private String name;
 
     public University(String name) {
-        this.name = name; //Ключевое слово, что бы использовать переменную с одним и тем же именем, указывает на переменную из класса, а не из метода.
+        this.name = name;
     }
 
     public List<Student> getStudentsWithGreatThanAverageKnowledge() {
-        int summaryKnowledgeLevel = 0; //Оглашаем переменную
-        for (Student student : students) { //Цикл для того, что бы взять значение уровня knowledge у студентов
-            Knowledge knowledge = student.getKnowledge(); //Записываем в переменную значение knowledge
-            Integer level = knowledge.getLevel(); //Записываем в переменную значение level
-            summaryKnowledgeLevel = level + summaryKnowledgeLevel; //Записываем в переменную общий уровень знаний всех студентов
-        }
-        float averageKnowledgeLevel = summaryKnowledgeLevel / (students.size()* 1.0f); //Считаем средний уровень
-        List<Student> studentsKnowledgeGreatAverage = new ArrayList<>();
-        for (Student student : students) { //Цикл для того, что бы выбрать студентов с уровнем знаний больше среднего
+        int summaryKnowledgeLevel = 0;
+        for (Student student : students) {
             Knowledge knowledge = student.getKnowledge();
             Integer level = knowledge.getLevel();
-            if (level > averageKnowledgeLevel) { //Условие для отбора
-                studentsKnowledgeGreatAverage.add(student); //Записываем студента с уровнем больше среднего в список
+            summaryKnowledgeLevel = level + summaryKnowledgeLevel;
+        }
+        float averageKnowledgeLevel = summaryKnowledgeLevel / (students.size()* 1.0f);
+        List<Student> studentsKnowledgeGreatAverage = new ArrayList<>();
+        for (Student student : students) {
+            Knowledge knowledge = student.getKnowledge();
+            Integer level = knowledge.getLevel();
+            if (level > averageKnowledgeLevel) {
+                studentsKnowledgeGreatAverage.add(student);
             }
         }
-        return studentsKnowledgeGreatAverage; //Возвращаем значение
+        return studentsKnowledgeGreatAverage;
     }
 
 
-    public void addStudent(Student student) { //Добавляем студентов в переменную
+    public void addStudent(Student student) {
         students.add(student);
     }
 
-    public String getName() { 
+    public String getName() {
         return name;
     }
 }
--- InternshipTest/src/main/Application.java
+++ InternshipTest/src/main/Application.java
@@ -1,30 +1,30 @@
-package main; //Пакет, обычно в названии соответствует физической структуре проекта.
+package main;
 
-import institution.University; //Подключаем(импортируем) внешние ресурсы
-import institution.interlink.Internship; //Подключаем(импортируем) внешние ресурсы
-import person.Student; //Подключаем(импортируем) внешние ресурсы
-import person.consciousness.Knowledge; //Подключаем(импортируем) внешние ресурсы
+import institution.University;
+import institution.interlink.Internship;
+import person.Student;
+import person.consciousness.Knowledge;
 
-import java.util.List; //Подключаем метод List, для работы со списками
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        University university = new University("CH.U.I."); //Создаем новый класс
-        Student andrewKostenko = new Student("Andrew Kostenko"); //Создаем новый класс
-        andrewKostenko.setKnowledge(new Knowledge(1)); //Задаем уровень знаний
-        university.addStudent(andrewKostenko); //Добавляем студента (andrewKostenko) в университет
-        Student juliaVeselkina = new Student("Julia Veselkina"); //Создаем новый класс
-        juliaVeselkina.setKnowledge(new Knowledge(4)); //Задаем уровень знаний
-        university.addStudent(juliaVeselkina); //Добавляем студента (juliaVeselkina) в университет
-        Student mariaPerechrest = new Student("Maria Perechrest"); //Создаем новый класс
-        mariaPerechrest.setKnowledge(new Knowledge(2)); //Задаем уровень знаний
-        university.addStudent(mariaPerechrest); //Добавляем студента (mariaPerechrest) в университет
+        University university = new University("CH.U.I.");
+        Student andrewKostenko = new Student("Andrew Kostenko");
+        andrewKostenko.setKnowledge(new Knowledge(1));
+        university.addStudent(andrewKostenko);
+        Student juliaVeselkina = new Student("Julia Veselkina");
+        juliaVeselkina.setKnowledge(new Knowledge(4));
+        university.addStudent(juliaVeselkina);
+        Student mariaPerechrest = new Student("Maria Perechrest");
+        mariaPerechrest.setKnowledge(new Knowledge(2));
+        university.addStudent(mariaPerechrest);
 
-        List<Student> students = university.getStudentsWithGreatThanAverageKnowledge(); //Присваевываем списку студентов метод для определения студентов, средний бал которых выше среднего.
+        List<Student> students = university.getStudentsWithGreatThanAverageKnowledge();
 
-        Internship internship = new Internship("Interlink"); //Создаем новый класс
-        internship.recruitStudents(students); //Проводим зачисление студентов в интенатуру.
-        System.out.println("List of internship's students:"); //Выводим текст
-        System.out.println(internship.getStudents()); //Выводим имена студентов, которые прошли в интернатуру
+        Internship internship = new Internship("Interlink");
+        internship.recruitStudents(students);
+        System.out.println("List of internship's students:");
+        System.out.println(internship.getStudents());
     }
 }
